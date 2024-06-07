e5cd4d1ceea76491d0c33774b8447bde626a8a64
gilangrmdhni <gilangrmdhni1003@gmail.com>
2024-06-07 13:34:55 +0700

    styling ui jilid 3

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -177,9 +177,9 @@ const HomePage = () => {
                                         </div>
                                     </Link>
                                 </div>
-                                <div className="w-full mt-6 sm:mt-12 mb-6 bg-gray-200 h-0.5 rounded-sm"></div>
-                                <h1 className="text-2xl font-semibold mb-4 ml-4">News</h1>
-                                <div className='flex flex-col items-center px-4'>
+                                <div className="w-full mt-6 sm:mt-12 mb-12 bg-gray-200 h-0.5 rounded-sm "></div>
+                                <h1 className="text-2xl font-semibold mb-4 ml-4">Berita</h1>
+                                <div className='flex flex-col items-center px-4 mb-20'>
                                     <NewsList />
                                 </div>
                             </div>
